77b5a4e7f050feef1148a8aa1bd5af72781e7648
Joe Bacal <bacalj@gmail.com>
2026-01-16 11:21:21 -0500

    chore: cleanup old docs and dead code

--- src/utils/session-utils.ts
+++ src/utils/session-utils.ts
@@ -19,17 +19,6 @@ export const generateSessionId = (): string => {
   return `qa_bot_session_${uuidv4()}`;
 };
 
-export const getOrCreateSessionId = (): string => {
-  const existingKey = Object.keys(localStorage).find(key => key.startsWith('qa_bot_session_'));
-  if (existingKey) {
-    const existingId = localStorage.getItem(existingKey);
-    return existingId || generateSessionId();
-  }
-
-  const newSessionId = generateSessionId();
-  localStorage.setItem(newSessionId, newSessionId);
-  return newSessionId;
-};
 
 /**
  * Get the session messages store from localStorage
